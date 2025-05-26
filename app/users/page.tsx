a00d21a58dd3bf88a15aaecc0e5cf1ce937c949e
ciandre <a.stanisic.codealkonost@gmail.com>
2025-05-26 21:16:40 +0200

    Implement user loading state and add API route for greeting message

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 const Users = async () => {
   const data = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await data.json();
-  console.log(users); // Log the fetched data to the console`
 
   return (
     <>
