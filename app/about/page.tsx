00499c1a07daef4241feeb57e8eddefae0e810db
ciandre <a.stanisic.codealkonost@gmail.com>
2025-05-26 14:13:37 +0200

    Add About and Contact pages with basic content

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a team dedicated to building amazing web applications.</p>
+      <p>
+        Our mission is to create user-friendly and efficient software solutions.
+      </p>
+      <p>Contact us for more information or collaboration opportunities!</p>
+    </div>
+  );
+}
